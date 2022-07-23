1ccd610646c8d5c8681bc6c4ee52bf34d8c3bf09
Hoangvu1520 <109757287+Hoangvu1520@users.noreply.github.com>
2022-07-23 23:01:48 +0700

    Add files via upload

--- ProjectStarter1/app/Models/User.php
+++ ProjectStarter1/app/Models/User.php
@@ -5,6 +5,8 @@ require_once('app/Models/Role.php');
 
 class User extends Model
 {
+    protected $table = 'users';
+    protected $fillable = ['id','role_id','email','password'];
     public function authenticate($data)
     {
         $email = $data['email'];
@@ -14,4 +16,16 @@ class User extends Model
         dd($sql);
         return $this->getFirst($sql);
     }
+    
+    public function validateRegister($data)
+    {
+        $email = $data['email'];
+        $password = $data['password'];
+        $roleAdminId = Role::ADMIN;
+        $sql = "INSERT into users(email, password) VALUES('$email','$password') AND password = md5('$password') AND role_id={$roleAdminId}";
+        dd($sql);
+        return $this->create($data);
+    }
+
+
 }
